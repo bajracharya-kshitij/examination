bf084f80df73b5f71598d572983e92f3b5430f48
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-12-04 08:02:13 +0545

    Take input from user and display message

--- src/com/javaexamples/Result.java
+++ src/com/javaexamples/Result.java
@@ -1,9 +1,14 @@
 package com.javaexamples;
 
+import java.util.Scanner;
+
 public class Result {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
+		System.out.print("Enter your name: ");
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		System.out.println("Hello " + name);
 	}
 	
 }
