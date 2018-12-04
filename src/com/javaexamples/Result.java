e7070c4302f44ae3c08da4cac30d3821a38ea915
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-12-04 15:22:38 +0545

    Calculate total and average marks

--- src/com/javaexamples/Result.java
+++ src/com/javaexamples/Result.java
@@ -16,12 +16,18 @@ public class Result {
 		int computerMarks = scanner.nextInt();
 		
 		System.out.print("Enter marks in Science: ");
-		int sciencehMarks = scanner.nextInt();
+		int scienceMarks = scanner.nextInt();
 		
 		scanner.close();
 		System.out.println("Hello " + name);
 		System.out.println("Your result:");
-		System.out.println(String.format("English %d, Computer %d and Science %d", englishMarks, computerMarks, sciencehMarks));
+		System.out.println(String.format("English %d, Computer %d and Science %d", englishMarks, computerMarks, scienceMarks));
+		
+		int total = englishMarks + computerMarks + scienceMarks;
+		System.out.println("Total marks received = " + total);
+		
+		int averageMarks = total/3;
+		System.out.println(String.format("Average marks = %d", averageMarks));
 	}
 	
 }
