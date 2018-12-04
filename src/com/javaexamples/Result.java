3e0c63c917a8c27f0a70550a56d7ce7a795901a5
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-12-04 15:36:58 +0545

    Add message for division

--- src/com/javaexamples/Result.java
+++ src/com/javaexamples/Result.java
@@ -35,6 +35,23 @@ public class Result {
 		} else {
 			System.out.println("Sorry, you've failed");
 		}
+		
+		/*
+		 * if average marks greater or equal to 80, distinction
+		 * else if average marks greater or equal to 60 and less than 80, first division
+		 * else if average marks greater or equal to 40 and less than 60, second division
+		 * else no division, failed
+		 * */
+		if(averageMarks >= 80) {
+			System.out.println("Excellent! You've attained Distinction");
+		} else if(averageMarks >= 60) {
+			System.out.println("Good! You've attained First Division");
+		} else if(averageMarks >= 40) {
+			System.out.println("Fair! You've attained Second Division");
+		} else {
+			System.out.println("Poor! You've attained no division because you failed");
+		}
+		
 	}
 	
 }
