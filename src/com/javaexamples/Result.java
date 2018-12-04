c88dc05c3179bcdb90c10dc0ed758af5d563f1d3
Kshitij Bajracharya <bajracharya.kshitij@gmail.com>
2018-12-04 22:44:38 +0545

    Organize code in functions

--- src/com/javaexamples/Result.java
+++ src/com/javaexamples/Result.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Result {
 
 	public static void main(String[] args) {
-		System.out.print("Enter your name: ");
 		Scanner scanner = new Scanner(System.in);
-		String name = scanner.nextLine();
 		
 		System.out.print("Enter number of subjects: ");
 		int numberOfSubjects = scanner.nextInt();
@@ -23,7 +21,11 @@ public class Result {
 		
 		scanner.close();
 		
-		System.out.println("Hello " + name);
+		displayMarks(numberOfSubjects, marksList, total);
+		calculateResult(total, numberOfSubjects);
+	}
+	
+	public static void displayMarks(int numberOfSubjects, int[] marksList, int total) {
 		System.out.println("Your result:");
 		
 		int count = 1;
@@ -33,7 +35,9 @@ public class Result {
 		}
 		
 		System.out.println("Total marks received = " + total);
-		
+	}
+	
+	public static void calculateResult(int total, int numberOfSubjects) {
 		float averageMarks = (float) total/numberOfSubjects;
 		System.out.println(String.format("Average marks = %f", averageMarks));
 		
@@ -43,23 +47,25 @@ public class Result {
 		} else {
 			System.out.println("Sorry, you've failed");
 		}
-		
+		displayDivision(averageMarks);
+	}
+	
+	public static void displayDivision(float avgMarks) {
 		/*
 		 * if average marks greater or equal to 80, distinction
 		 * else if average marks greater or equal to 60 and less than 80, first division
 		 * else if average marks greater or equal to 40 and less than 60, second division
 		 * else no division, failed
 		 * */
-		if(averageMarks >= 80) {
+		if(avgMarks >= 80) {
 			System.out.println("Excellent! You've attained Distinction");
-		} else if(averageMarks >= 60) {
+		} else if(avgMarks >= 60) {
 			System.out.println("Good! You've attained First Division");
-		} else if(averageMarks >= 40) {
+		} else if(avgMarks >= 40) {
 			System.out.println("Fair! You've attained Second Division");
 		} else {
 			System.out.println("Poor! You've attained no division because you failed");
 		}
-		
 	}
 	
 }
